bbbcf5551eddbf65eee5f06ddc8e551906edfe6d
victor <victor@aresmaterials.com>
2024-04-03 17:40:00 -0600

    smol changes

--- particles_animation.py
+++ particles_animation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import Normalize
+#from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from itertools import combinations
 
@@ -31,7 +32,6 @@ def set_box_limits(ax_object,final_configuration,limits_scheme = 'fit_final'):
 
     if limits_scheme=='loose':
         return ax_object
-
     N = len(final_configuration)//3
     final_config_matrix = np.reshape(final_configuration, (N,3))
     ax_object.set_xlim(min(final_config_matrix[:,0]),max(final_config_matrix[:,0]))
@@ -39,31 +39,53 @@ def set_box_limits(ax_object,final_configuration,limits_scheme = 'fit_final'):
     ax_object.set_zlim(min(final_config_matrix[:,2]),max(final_config_matrix[:,2]))
     return ax_object
 
-def visualize_particle_system(vectors, number_of_particles, filename='particle_animation.gif',dimensions=3):
+def create_colormap(energies, vmin=None, vmax=None, cmap='hot'):
+    if not vmin:
+        vmin=min(energies)
+    if not vmax:
+        vmax=max(energies)
+    norm = Normalize(vmin=vmin, vmax=vmax)
+    colormap = plt.get_cmap(cmap)
+    return norm, colormap
+
+def initialize_plotting_elements(axes_object,vectors,dimensions,number_of_particles,particle_size=100):
+    ### Create the elements on the plot that will be updated
+    first_frame = np.array(vectors[0]).reshape(number_of_particles, dimensions)
+    scatter_object = axes_object.scatter(first_frame[:, 0], first_frame[:, 1], first_frame[:, 2], s=particle_size)
+
+    # Just dummy lines for later updating
+    number_of_lines = number_of_particles*(number_of_particles-1)//2
+    lines = [axes_object.plot([0, 0], [0, 0], [0, 0], 'gray', lw=0.5, alpha=0.3)[0] for _ in range(number_of_lines)]
+
+    # Dummy text and formatting
+    time_text = axes_object.text2D(0.00, 0.95, '', transform=axes_object.transAxes)
+    return scatter_object, lines, time_text
+
+def visualize_particle_system(vectors, energies, number_of_particles, filename='particle_animation.gif',dimensions=3):
     ### Create the plot canvas
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set_title('Best individual configuration')
+    normalizer,colormap = create_colormap(energies,vmin= -12.71,vmax=10) 
     
     # some styling
     ax = plotting_plane_style(ax)
     ax = set_box_limits(ax, vectors[-1],limits_scheme='fit_final')
-    
-    first_frame = np.array(vectors[0]).reshape(number_of_particles, dimensions)
-    scatter_object = ax.scatter(first_frame[:, 0], first_frame[:, 1], first_frame[:, 2], s=100)
-    lines = [ax.plot([0, 0], [0, 0], [0, 0], 'gray', lw=0.5, alpha=0.3)[0] for _ in range(number_of_particles*(number_of_particles-1)//2)]
 
-    time_text = ax.text2D(0.00, 0.95, '', transform=ax.transAxes)
+    scatter_object, lines, time_text = initialize_plotting_elements(ax,vectors,dimensions,number_of_particles)
 
     def update(frame_number):
-        data = np.array(vectors[frame_number]).reshape(number_of_particles, dimensions)
-        scatter_object._offsets3d = (data[:, 0], data[:, 1], data[:, 2])
+        positions = np.array(vectors[frame_number]).reshape(number_of_particles, dimensions)
+        scatter_object._offsets3d = (positions[:, 0], positions[:, 1], positions[:, 2])
+
+        color = colormap(normalizer(energies[frame_number]))
+        scatter_object.set_color(color)
 
         for line, (i, j) in zip(lines, combinations(range(number_of_particles), 2)):
-            line.set_data(np.array([data[i, 0], data[j, 0]]), np.array([data[i, 1], data[j, 1]]))
-            line.set_3d_properties(np.array([data[i, 2], data[j, 2]]))
+            line.set_data(np.array([positions[i, 0], positions[j, 0]]), np.array([positions[i, 1], positions[j, 1]]))
+            line.set_3d_properties(np.array([positions[i, 2], positions[j, 2]]))
 
-        time_text.set_text(f'Generation: {frame_number}')
+        time_text.set_text(f'Generation: {frame_number}. Energy: {round(energies[frame_number],3)}')
 
         return [scatter_object, *lines, time_text]
 
