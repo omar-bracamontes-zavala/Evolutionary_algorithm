822684e098459fdf42a10a552819e2943e60dd7a
victor <victor@aresmaterials.com>
2024-04-12 12:24:48 -0600

    added function for calculating performance metrics

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -2,6 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import evolutionary_functions as ef
 from evolutionary_functions import fitness
+from multiprocess import Pool
+from time import time
+
+#
+# Evaluation functions
+#
+
+def calculate_performance_metrics(evolutionary_algorithm, evolutionary_functions_set, sample_size = 20):
+    '''
+    Calculates performance metrics over several runs.
+    This function generates child processes.
+    input:
+        evolutionary_algorithm (func): Main function for the execution of the evolutive algorithm.
+        evolutionary_functions_set (list): Ordered arguments for the evolutionary algorithm to be evaluated.
+        sample_size (int): Number of samples to run.
+    returns
+        Dictionary of metrics from the run.
+    '''
+    def evolution_evaluation_wrapper(*args):
+        starting_time = time()
+        best_fitness_history, best_individuals_history = evolutionary_algorithm(*evolutionary_functions_set)
+        return {'best_fitness':best_fitness_history[-1], 'runtime':(time()-starting_time)}
+
+    with Pool() as pool:
+        best_fitnesses = pool.map(evolution_evaluation_wrapper , [None]*sample_size)
+    mean_best_fitness = np.mean([trial['best_fitness'] for trial in best_fitnesses])
+    return {'MBF': mean_best_fitness}
+
 
 #
 # Helpers
@@ -100,6 +128,26 @@ if __name__=='__main__':
     mutation_rate = 0.01
     mutation_strength = 1. # this could has an adaptative control 
     
+    ###########
+    # For evaluation testing
+    # # this fker has to have same order of args as in the definition for now
+    # evolutionary_args = [
+    #     fitness,
+    #     initialize_population,
+    #     evaluate_population,
+    #     select_parents,
+    #     crossover,
+    #     mutate,
+    #     generate_new_population,
+    #     population_size,
+    #     number_of_particles,
+    #     max_generations,
+    #     mutation_rate,
+    #     mutation_strength
+    #     ]
+    
+    # print(calculate_performance_metrics(evolutionary_algorithm,evolutionary_args))
+    ############
 
     # Run the algorithm
     best_fitness_history, best_individuals_history = evolutionary_algorithm(
